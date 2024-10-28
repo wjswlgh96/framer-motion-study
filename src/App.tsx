7af386ed13c96e16e179ef86acd8cd5bc53b7e70
JiHo Jeon <wjswlgh96@naver.com>
2024-10-28 14:26:15 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { motion, Variants } from "framer-motion";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 20px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
+`;
+
+const Circle = styled(motion.div)`
+  background: white;
+  width: 70px;
+  height: 70px;
+  border-radius: 100%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
+  place-self: center;
+`;
+
+const boxVariants: Variants = {
+  start: { opacity: 0 },
+  end: {
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants: Variants = {
+  start: { opacity: 0, y: 30 },
+  end: { opacity: 1, y: 0 },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Box variants={boxVariants} initial={"start"} animate={"end"}>
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
+    </Wrapper>
   );
 }
 
---
+++ src/themes/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "#111",
+};
+
+export { theme };
