30568c3d411351beae65c3b97e40a4e8e32dd763
JiHo Jeon <wjswlgh96@naver.com>
2024-10-28 17:10:28 +0900

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,65 @@
 import styled from "styled-components";
-import { motion, Variants } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
+const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 15px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
   border-radius: 20px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
+  height: 200px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.5);
 `;
 
-const Circle = styled(motion.div)`
-  background: white;
-  width: 70px;
-  height: 70px;
-  border-radius: 100%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
-  place-self: center;
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
-
-const boxVariants: Variants = {
-  start: { opacity: 0 },
-  end: {
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const circleVariants: Variants = {
-  start: { opacity: 0, y: 30 },
-  end: { opacity: 1, y: 0 },
-};
 
 function App() {
+  const [id, setId] = useState<string | null>(null);
+
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial={"start"} animate={"end"}>
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Grid>
+        {["1", "2", "3", "4"].map((i) => (
+          <Box onClick={() => setId(i)} key={i} layoutId={i} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            onClick={() => setId(null)}
+            initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+          >
+            <Box layoutId={id} style={{ width: 400 }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
